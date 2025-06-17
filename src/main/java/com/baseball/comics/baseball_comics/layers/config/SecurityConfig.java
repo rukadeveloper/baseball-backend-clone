71d3d22c69457f9d3ca7eb994147a860d0cd9b79
rukadeveloper <kansaki135@gmail.com>
2025-06-17 13:39:46 -0700

    configs

--- src/main/java/com/baseball/comics/baseball_comics/layers/config/SecurityConfig.java
+++ src/main/java/com/baseball/comics/baseball_comics/layers/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 주소
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://rukadeveloper.github.io")); // 프론트엔드 주소
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
