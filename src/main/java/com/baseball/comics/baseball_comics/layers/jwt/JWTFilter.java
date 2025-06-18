5c183ce111405f19030ab59a5f6d6bcd9868da53
rukadeveloper <kansaki135@gmail.com>
2025-06-17 19:02:26 -0700

    오류해결

--- src/main/java/com/baseball/comics/baseball_comics/layers/jwt/JWTFilter.java
+++ src/main/java/com/baseball/comics/baseball_comics/layers/jwt/JWTFilter.java
@@ -16,16 +16,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorization = request.getHeader("Authorization");
 
-        if(authorization == null || authorization.startsWith("Bearer")) {
+        if(authorization == null || !authorization.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
